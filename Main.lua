a7b0f777956f2f3b234e4512fe2cebe4a277f994
MaxsModMenu <ngytwhrmh2@privaterelay.appleid.com>
2025-10-08 20:51:41 +1300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,11 +2,18 @@ local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMen
 local fly = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/fly.lua"))()
 local noclip = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/noclip.lua"))()
 local walkspeed = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/walkspeed.lua"))()
-local allay = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/allay.lua"))()
+local max = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/max.lua"))()
+local flingall = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/flingall.lua"))()
+local esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/esp.lua"))()
+local gettools = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxsModMenu/MaxsModMenus/main/gettools.lua"))()
 
 ui:Create({
     onFly = fly.Toggle,
     onNoclip = noclip.Toggle,
     onWalkspeed = walkspeed.Toggle,
-    onAllay = allay.Spawn
+    onMax = max.Spawn,
+    onFlingAll = flingall.Activate,
+    onESP = esp.Enable,
+    onGetTools = gettools.Grab
 })
+
